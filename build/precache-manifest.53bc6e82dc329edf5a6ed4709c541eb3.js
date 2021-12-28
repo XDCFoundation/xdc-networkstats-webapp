d5bcc06dce624148d7d5754b763eed5e33956f70
deepali <deepali@leewayhertz.com>
2021-12-28 18:45:08 +0530

    build

---
+++ build/precache-manifest.53bc6e82dc329edf5a6ed4709c541eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7dd524f87087f490d7f19c5e040835",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12420417d14292f256ae",
+    "url": "/static/css/2.a95331b8.chunk.css"
+  },
+  {
+    "revision": "ea058d864b894550ba9c",
+    "url": "/static/css/main.e5c328c6.chunk.css"
+  },
+  {
+    "revision": "12420417d14292f256ae",
+    "url": "/static/js/2.f766498e.chunk.js"
+  },
+  {
+    "revision": "ea058d864b894550ba9c",
+    "url": "/static/js/main.99d9e7c4.chunk.js"
+  },
+  {
+    "revision": "e614aacbc12995b59ed9",
+    "url": "/static/js/runtime~main.b1185d1c.js"
+  }
+]);
