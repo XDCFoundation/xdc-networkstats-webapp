b7316d69addf61dd5f36085b8b010cdd77e6b8a2
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-05-09 17:41:09 +0530

    Build prod

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4533c2b3478f4eb2f2f3d143bee859b5.js"
+  "/precache-manifest.4c4687f83742eecf7c02495ef3dfad2b.js"
 );
 
 self.addEventListener('message', (event) => {
