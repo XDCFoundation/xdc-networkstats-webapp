c1d34c4e707567effa6adcfb16c025e755760a01
Sourabh_SourabhArora <sourabh@leewayhertz.com>
2022-05-12 17:52:41 +0530

    Build prod

---
+++ build/precache-manifest.42c3fe7ab7df8a236a631d1f9a04925e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e078356d2024f149a1dd6c73546ac895",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622cd2f4d8f21ca81620",
+    "url": "/static/css/2.a95331b8.chunk.css"
+  },
+  {
+    "revision": "3fd1ebc6ef430df1a391",
+    "url": "/static/css/main.05b4dccd.chunk.css"
+  },
+  {
+    "revision": "622cd2f4d8f21ca81620",
+    "url": "/static/js/2.41714b90.chunk.js"
+  },
+  {
+    "revision": "3fd1ebc6ef430df1a391",
+    "url": "/static/js/main.bb16c689.chunk.js"
+  },
+  {
+    "revision": "e614aacbc12995b59ed9",
+    "url": "/static/js/runtime~main.b1185d1c.js"
+  }
+]);
